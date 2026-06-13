3dd52e90b4ad0fab059ab40921a440b4aabeaa63
EpicSenpai <arvindrathod5476@gmail.com>
2026-06-13 22:57:32 +0530

    Update approve.py

--- plugins/approve.py
+++ plugins/approve.py
@@ -61,7 +61,7 @@ async def autoapprove(client, message: ChatJoinRequest):
         
         await client.send_photo(
             chat_id=user.id,
-            photo='https://telegra.ph/file/f3d3aff9ec422158feb05-d2180e3665e0ac4d32.jpg',
+            photo='https://litter.catbox.moe/mzsd3o.jpg',
             caption=caption,
             reply_markup=markup
         )
